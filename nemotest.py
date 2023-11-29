e2eda1729c536918835f38609f53b8d30ea9c06e
security_bot <132981588+mushroomtest@users.noreply.github.com>
2023-11-29 14:58:52 +0800

    Update nemotest.py

--- nemotest.py
+++ nemotest.py
@@ -1,3 +1,7 @@
 url = https://abc.bytedance.net/agent5
 ak = 'AKLTMGMzYmEzYWI3N2ZlNGNhZjhjODMwMDA4YjklZLQwOWY'
 sk = 'NeubqldeubuanuiOHuebu3Lnuefl2swinLnuefklxwom=='
+
+url = https://bcd.bytedance.net/agent5
+ak = 'AKLTMGMzYmEzYWI3N2ZlNGMhZjhjODMwMDA4YjklZLQwOWY'
+sk = 'NeubqldeubuanuiOHuebu3Lnuefl2swinLmuefklxwom=='
