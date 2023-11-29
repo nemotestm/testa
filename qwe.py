53ecab4ffdb105639b5d421529d698e9b057aeb6
security_bot <132981588+mushroomtest@users.noreply.github.com>
2023-11-29 14:55:06 +0800

    Update qwe.py

--- qwe.py
+++ qwe.py
@@ -1,1 +1,6 @@
 nnea.bytedance.net
+
+mmea.bytedance.net
+url = https://mmea.bytedance.net/agent5
+ak = 'AKLTMGMcYmEzYWI3N2ZlNGNhZjhjODMwMDA4YjklZTQwOWY'
+sk = 'NeubqldeubuanuiOHuebu3Lnuefl2swinLnuefklxwom=='
